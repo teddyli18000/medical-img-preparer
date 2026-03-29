ac3c9c7ceece4e8d3d5ea166dd72cef1fc9ab56b
Teddy <teddyli18000@gmail.com>
2026-03-29 16:23:05 +0800

    docs

--- data_prep/config_prep.py
+++ data_prep/config_prep.py
@@ -4,7 +4,7 @@ import os
 class ConfigPrep:
     # --- 路径配置 (请在此处填写您的本地路径) ---
     # [Ref. 3, Section: Data Pipelines] - 离线预处理建议与代码逻辑解耦
-    RAW_DATA_DIR = r"E:\Your_Path_To_Raw_Data\Task07_Pancreas"  # TODO: 填入原数据集路径
+    RAW_DATA_DIR = r"E:\Python_Projects\Swin-UNETR-for-MSD-task7\data\Task07_Pancreas"  # TODO: 填入原数据集路径
 
     # 程序一生成数据的存放位置
     OUTPUT_DIR = os.path.abspath(os.path.join(
